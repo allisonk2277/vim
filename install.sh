6ae503e7c84ddf9b7a50e5b5dfdc60f52439998d
Daniel Kong <dkong@google.com>
2015-09-24 13:07:05 -0700

    Use $HOME instead of ~
    - It's generally the more correct thing to do.

--- install.sh
+++ install.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
 # Make links for .vimrc and .gvimrc.
-rm -f ~/.vimrc ~/.gvimrc ~/.ideavimrc
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-ln -s ~/.vim/ideavimrc ~/.ideavimrc
+rm -f $HOME/.vimrc $HOME/.gvimrc $HOME/.ideavimrc
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
+ln -s $HOME/.vim/ideavimrc $HOME/.ideavimrc
 
 # Install Vundle.
-rm -rf ~/.vim/bundle/vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+rm -rf $HOME/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
 # Install plugins.
 vim +BundleInstall +qall
 
 # Install custom fonts.
 if [ "$(uname)" == "Linux" ]; then
-  echo "Installing custom fonts to ~/.fonts ..."
-  if [ ! -d ~/.fonts ]; then
-    mkdir -p ~/.fonts
+  echo "Installing custom fonts to $HOME/.fonts ..."
+  if [ ! -d $HOME/.fonts ]; then
+    mkdir -p $HOME/.fonts
   fi
-  cp ~/.vim/fonts/* ~/.fonts
-  fc-cache -vf ~/.fonts > /dev/null
+  cp $HOME/.vim/fonts/* $HOME/.fonts
+  # Update font cache
+  fc-cache -vf $HOME/.fonts > /dev/null
 # OS X
 elif [ "$(uname)" == "Darwin" ]; then
-  echo "Installing custom fonts to ~/Library/Fonts ..."
-  if [ ! -d ~/Library/Fonts ]; then
-    mkdir -p ~/Library/Fonts
+  echo "Installing custom fonts to $HOME/Library/Fonts ..."
+  if [ ! -d $HOME/Library/Fonts ]; then
+    mkdir -p $HOME/Library/Fonts
   fi
-  cp ~/.vim/fonts/* ~/Library/Fonts
+  cp $HOME/.vim/fonts/* $HOME/Library/Fonts
 else
   echo "Not running Linux or OS X, so not installing custom fonts."
 fi
